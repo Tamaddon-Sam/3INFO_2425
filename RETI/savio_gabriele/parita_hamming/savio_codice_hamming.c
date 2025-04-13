18d002e656f2dbc3858eca31b6d90e7dabed95a7
Savio-Gabriele <gabriele.savio@istitutoagnelli.it>
2025-04-13 16:19:07 +0200

    Codice di parita e di hamming

---
+++ RETI/savio_gabriele/parita_hamming/savio_codice_hamming.c
@@ -0,0 +1,50 @@
+/*
+scrivere un programma in C che prenda dalla linea di comando due stringhe binarie e calcoli e visualizzi sulla console la distanza di hamming
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LENGTH 100
+
+int distanza_hamming(const char *stringa1, const char *stringa2)
+{
+    int distanza = 0;
+    int len = strlen(stringa1);
+    if (len != strlen(stringa2))
+    {
+        printf("Errore, le stringhe devono essere della stessa lunghezza\n");
+        return -1;
+    }
+    for (int i = 0; i < len; i++)
+    {
+        if (stringa1[i] != stringa2[i])
+        {
+            distanza++;
+        }
+    }
+    return distanza;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        return 1;
+    }
+
+    const char *stringa1 = argv[1];
+    const char *stringa2 = argv[2];
+
+    printf("Stringa 1: %s\n", stringa1);
+    printf("Stringa 2: %s\n", stringa2);
+
+    int distanza = distanza_hamming(stringa1, stringa2);
+    if (distanza != -1)
+    {
+        printf("Distanza di hamming: %d\n", distanza);
+    }
+
+    return 0;
+}
